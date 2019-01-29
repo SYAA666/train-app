33f87916428720f0a3115f870c73d5cfca1aaa75
Mikalai Hlushakou <Mikalai_Hlushakou@epam.com>
2019-01-29 13:05:09 +0300

    form add another person

--- src/app/forms/forms.component.ts
+++ src/app/forms/forms.component.ts
@@ -1,6 +1,8 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { FormBuilder, Validators } from '@angular/forms';
 import { Router } from '@angular/router';
+import {PeopleService} from '../services/people.service';
+import {User} from '../user';
 
 
 @Component({
@@ -8,12 +10,14 @@ import { Router } from '@angular/router';
   templateUrl: './forms.component.html',
   styleUrls: ['./forms.component.css'],
 })
-export class FormsComponent {
+export class FormsComponent implements OnInit {
+  transferData: User;
   newUser = this.fb.group({
     title: this.fb.group({
       firstName: ['', Validators.required],
       lastName: [''],
-      id: ['']
+      id: [''],
+      age: ['']
     }),
     location: this.fb.group({
       city: [''],
@@ -26,13 +30,26 @@ export class FormsComponent {
     }),
     gender: ['']
   });
-  constructor(private fb: FormBuilder, private router: Router) { }
+  constructor(private fb: FormBuilder, private router: Router, private peopleService: PeopleService ) { }
+  ngOnInit(): void {
+    console.log(this.peopleService.usersCollectionHolder);
+  }
 
   onSubmit() {
+    this.transferData = {
+      name: this.newUser.value.title.firstName + ' ' + this.newUser.value.title.lastName,
+      gender: this.newUser.value.gender,
+      age: this.newUser.value.title.age,
+      login: this.newUser.value.personal.userName,
+      phone: this.newUser.value.personal.phone,
+      image: 'https://pixel.nymag.com/imgs/daily/intelligencer/2014/06/04/04-slenderman-3.w700.h700.jpg',
+      id: this.newUser.value.title.id,
+      location: this.newUser.value.location,
+      email: this.newUser.value.personal.email
+    };
+    this.peopleService.usersCollectionHolder.push(this.transferData);
     console.log('submitted');
     console.log(this.newUser.value);
-    if(this.newUser.status === 'valid' ) {
-      this.router.navigate(['/app-people']);
-    }
+    this.router.navigate(['/app-people']);
   }
 }
